2aa7a8260bf34cbdc53b90277480c71fde9b45ef
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-02-06 19:42:51 +1000

    Modify password_stars.py, rename some functions.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,15 +2,15 @@ MINIMUM_PASSWORD_LENGTH = 8
 
 
 def main():
-    password = get_password()
-    display_password_in_asterisks(password)
+    password = get_valid_password()
+    print(turn_password_to_asterisks(password))
 
 
-def display_password_in_asterisks(password):
-    print(len(password) * "*")
+def turn_password_to_asterisks(password):
+    return len(password) * "*"
 
 
-def get_password():
+def get_valid_password():
     password = input("Enter your password: ")
     while len(password) < MINIMUM_PASSWORD_LENGTH:
         print("Password must be at least {} symbols.".format(MINIMUM_PASSWORD_LENGTH))
