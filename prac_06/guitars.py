a1c937c097771fe3426e3dc2d46b12e323c0c78e
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-03-03 10:54:44 +0800

    Remove test lines

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,8 +5,8 @@ def main():
     guitars = []
     print('My guitars!')
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     guitar_name = input('Name: ')
     while guitar_name != '':
