eef0b73e2f5c93e5126cf83547f608328f9e1f93
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-03-19 09:22:32 +1000

    Update functions for getting valid inputs

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -30,22 +30,34 @@ def main():
         if menu_choice == 'f':
             pass
         if menu_choice == 'a':
-            pass
+            print("Let's add a new project")
+            project_name = get_valid_text('Name: ')
+            project_start_date = get_valid_text('Start date (dd/mm/yy): ')
+            project_priority = get_valid_priority('Priority: ')
+            project_cost_estimate = get_valid_text('Cost estimate: ')
+            project_percentage = get_valid_percentage('Completion percentage: ')
+
+            print()
         if menu_choice == 'u':
             # Display projects with index:
             incomplete_project = projects['Incomplete']
             for index, project in enumerate(incomplete_project):
                 print(index, project)
-            project_choice = get_valid_ranged_number(
-                end=len(projects['Incomplete']),
-                prompt='Project Choice: '
+            project_choice = get_valid_number(
+                0,
+                len(projects['Incomplete']) - 1,
+                'Project Choice: '
             )
             chosen_project = incomplete_project[project_choice]
             print(chosen_project)
-            new_percentage = get_valid_ranged_number(end=101, prompt='New Percentage:')
-            new_priority = get_valid_ranged_number(end=11, prompt='New Priority:')
-            chosen_project.completion_percentage = new_percentage
-            chosen_project.priority = new_priority
+            # Ask for new inputs
+            new_percentage = get_valid_percentage('New Percentage: ')
+            new_priority = get_valid_priority('New Priority: ')
+            # Apply changes when inputs are detected
+            if new_priority:
+                chosen_project.priority = new_priority
+            if new_percentage:
+                chosen_project.completion_percentage = new_percentage
 
         print(MENU)
         menu_choice = input('>>> ').strip().lower()
@@ -53,13 +65,47 @@ def main():
     print('Thank you for using custom-built project management software.')
 
 
-def get_valid_ranged_number(start=0, end=100, prompt='>>> '):
-    """To avoid unwanted choice, return valid project choice"""
-    project_choice = get_valid_number(prompt)
-    while project_choice not in range(start, end):
-        print('Invalid, out of range.')
-        project_choice = get_valid_number('Project Choice: ')
-    return project_choice
+def get_valid_percentage(prompt):
+    return get_valid_number(0, 100, prompt, 'int', True)
+
+
+def get_valid_priority(prompt):
+    return get_valid_number(0, 10, prompt, 'int', True)
+
+
+def get_valid_text(prompt):
+    text = input(prompt).strip()
+    while text == '':
+        print('Input cannot be empty')
+        text = input(prompt).strip()
+    return text
+
+
+def get_valid_number(start, end, prompt, number_type='int', allow_empty=False):
+    """Return valid number"""
+    number_type = number_type.strip().lower()
+    is_valid = False
+    number = None
+    while not is_valid:
+        number = input(prompt).strip()
+        if allow_empty and not len(number):
+            return None
+        try:
+            # Type selection
+            if number_type == 'float':
+                number = float(number)
+            elif number_type == 'int':
+                number = int(number)
+
+            if number < start:
+                print(f'Number must be >= than {start}')
+            elif number > end:
+                print(f'Number must be <= {end}')
+            else:
+                is_valid = True
+        except ValueError:
+            print('Invalid! Must input a valid number')
+    return number
 
 
 def load_projects(filename):
@@ -81,28 +127,6 @@ def load_projects(filename):
             else:
                 categorized_projects['Incomplete'].append(project)
     return categorized_projects
-
-
-def get_valid_number(prompt, number_type='int'):
-    """Return valid number"""
-    number_type = number_type.strip().lower()
-    is_valid = False
-    number = None
-    while not is_valid:
-        try:
-            # Type selection
-            if number_type == 'float':
-                number = float(input(prompt))
-            elif number_type == 'int':
-                number = int(input(prompt))
-
-            if number < 0:
-                print('Number must be greater than 0')
-            else:
-                is_valid = True
-        except ValueError:
-            print('Invalid! Must input a valid number')
-    return number
 
 
 if __name__ == '__main__':
