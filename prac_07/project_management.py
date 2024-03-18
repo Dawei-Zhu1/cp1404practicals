a1593477d859f16685b364d50ccd8727182fe88f
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-03-18 23:10:42 +1000

    Finish project display

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -12,25 +12,36 @@ MENU = """- (L)oad projects
 
 def main():
     # Initials
-    projects = []
     # Read data from project.txt
+    projects = load_projects(FILENAME)
     print(MENU)
     choice = input('>>> ').strip().lower()
     while choice != 'q':
         if choice == 'l':
-            projects = load_projects(FILENAME)
+            pass
         if choice == 's':
             pass
         if choice == 'd':
-            for i in projects:
-                print(i)
+            for category in projects:
+                print(f'{category}:')
+                for project in projects[category]:
+                    print(f'  {project}')
+
+        if choice == 'f':
+            pass
+        if choice == 'a':
+            pass
+        if choice == 'u':
+            pass
         print(MENU)
         choice = input('>>> ').strip().lower()
+    # Quit
+    print('Thank you for using custom-built project management software.')
 
 
 def load_projects(filename):
     with open(filename, 'r') as f:
-        project_list = []
+        categorized_projects = {'Incomplete':[], 'Completed':[]}
         # Skip the titles
         f.readline()
         # Process contents
@@ -41,10 +52,12 @@ def load_projects(filename):
             priority = int(parts[2])
             cost_estimate = float(parts[3])
             completion_percentage = int(parts[4])
-            project_list.append(
-                Project(project_name, start_date, priority, cost_estimate, completion_percentage)
-            )
-    return project_list
+            project = Project(project_name, start_date, priority, cost_estimate, completion_percentage)
+            if completion_percentage == 100:
+                categorized_projects['Completed'].append(project)
+            else:
+                categorized_projects['Incomplete'].append(project)
+    return categorized_projects
 
 
 if __name__ == '__main__':
